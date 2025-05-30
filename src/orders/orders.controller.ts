5baf7908b09231a79b0138ba6c292f86306035f3
Jorge Avella <79177900+jaavella07@users.noreply.github.com>
2025-05-29 23:16:18 -0500

    Add common and docker

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,35 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, ParseIntPipe } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { PaginationDto } from 'src/common';
 
 @Controller()
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
-  @MessagePattern('createOrder')
+    // @Post()
+  @MessagePattern({ cmd: 'create_order' })
   create(@Payload() createOrderDto: CreateOrderDto) {
     return this.ordersService.create(createOrderDto);
   }
 
-  @MessagePattern('findAllOrders')
-  findAll() {
+
+  @MessagePattern({ cmd: 'find_all_order' })
+  findAll( @Payload() paginationDto: PaginationDto) {
     return this.ordersService.findAll();
   }
 
-  @MessagePattern('findOneOrder')
-  findOne(@Payload() id: number) {
-    return this.ordersService.findOne(id);
+  @MessagePattern({ cmd: 'find_one_order' })
+  findOne(@Payload('id', ParseIntPipe) id: string) {
+    return this.ordersService.findOne(+id);
   }
 
-  @MessagePattern('updateOrder')
-  update(@Payload() updateOrderDto: UpdateOrderDto) {
-    return this.ordersService.update(updateOrderDto.id, updateOrderDto);
-  }
-
-  @MessagePattern('removeOrder')
-  remove(@Payload() id: number) {
-    return this.ordersService.remove(id);
-  }
 }
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
-import { UpdateOrderDto } from './dto/update-order.dto';
+
 
 @Injectable()
 export class OrdersService {
+
+
   create(createOrderDto: CreateOrderDto) {
     return 'This action adds a new order';
   }
@@ -16,11 +18,4 @@ export class OrdersService {
     return `This action returns a #${id} order`;
   }
 
-  update(id: number, updateOrderDto: UpdateOrderDto) {
-    return `This action updates a #${id} order`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} order`;
-  }
 }
