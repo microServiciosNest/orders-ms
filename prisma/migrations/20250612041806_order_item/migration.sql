23ac5a96118312c28cac9b434ba8425cf58fbacc
Jorge Avella <79177900+jaavella07@users.noreply.github.com>
2025-06-11 23:32:31 -0500

    Add OrderItem DTO's

---
+++ prisma/migrations/20250612041806_order_item/migration.sql
@@ -0,0 +1,12 @@
+-- AlterEnum
+ALTER TYPE "OrderStatus" ADD VALUE 'DELIVERED';
+
+-- CreateTable
+CREATE TABLE "OrderItem" (
+    "id" TEXT NOT NULL,
+    "productId" INTEGER NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+
+    CONSTRAINT "OrderItem_pkey" PRIMARY KEY ("id")
+);
--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,26 +1,34 @@
-import { OrderStatus } from "@prisma/client";
-import { IsBoolean, IsEnum, IsInt, IsNumber, IsOptional, IsPositive, IsString } from "class-validator";
-import { OrderStatusList } from "./enum/order.enum";
+
+import { ArrayMinSize, IsArray, ValidateNested } from "class-validator";
+import { Type } from "class-transformer";
+import { OrderItemDto } from "./order-item.dto";
 
 export class CreateOrderDto {
 
+    // ANTES DE LA CREACION DEL MICROSERVICIO
+    // @IsNumber()
+    // @IsPositive()
+    // totalAmount: number;
 
-    @IsNumber()
-    @IsPositive()
-    totalAmount: number;
+    // @IsNumber()
+    // @IsPositive()
+    // totalItems: number;
 
-    @IsNumber()
-    @IsPositive()
-    totalItems: number;
-
-    @IsEnum(OrderStatusList, {
-        message: `Possible status values are ${OrderStatusList}`
-    })
-    @IsOptional()
-    status: OrderStatus = OrderStatus.PENDING;
+    // @IsEnum(OrderStatusList, {
+    //     message: `Possible status values are ${OrderStatusList}`
+    // })
+    // @IsOptional()
+    // status: OrderStatus = OrderStatus.PENDING;
     
-    @IsBoolean()
-    @IsOptional()
-    paid: boolean = false;
+    // @IsBoolean()
+    // @IsOptional()
+    // paid: boolean = false;
+
+    @IsArray()
+    @ArrayMinSize(1)
+    @ValidateNested({ each: true })
+    @Type(() => OrderItemDto )
+    item: OrderItemDto[]
+
 
 }
